0b6be48dba4048958a4642c8e221f47161e72417
Abdale Hassan <cabdale989@gmail.com>
2021-06-13 11:30:58 -0500

    add a get route to find all the categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,46 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findByPk } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try{
+    const categoryData = await Category.findAll({
+      include:[
+        {
+          model: Product,
+        }
+      ]
+    });
+    res.status(200).json(categoryData);
+   }catch{
+     res.status(500).json(err);
+   }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
   // create a new category
+ 
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',  (req, res) => {
   // update a category by its `id` value
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',  (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
