ba891734f80f6ff46ebc482a6b2fc5d908485dde
Abdale Hassan <cabdale989@gmail.com>
2021-06-13 18:49:17 -0500

    add a get route to find all products, and to find one product, and add a post route and delete route find create products and delete one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -49,7 +49,15 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/',(req, res) => {
+  Product.create(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,  
+      stock: req.body.stock,
+      tagIds: req.body.tagIds,
+    }
+  )
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -122,8 +130,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
