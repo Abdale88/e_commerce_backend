4d28b4ecec8eb10efe8a20dee22814f1238581ba
Abdale Hassan <cabdale989@gmail.com>
2021-06-13 11:48:57 -0500

    return category for the user

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,6 +30,7 @@ router.get('/:id', async (req, res) => {
     if(!categoryData){
       res.status(404).json({message: 'No category found with this id!'});
     }
+    res.status(200).json(categoryData);
   } catch (error) {
     res.status(500).json(error)
   }
@@ -78,5 +79,6 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json(error)
   }
 });
+
 
 module.exports = router;
