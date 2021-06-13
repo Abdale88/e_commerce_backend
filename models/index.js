4484b3c81a384ff1dbfe06f6465d4932311b802f
Abdale Hassan <cabdale989@gmail.com>
2021-06-12 19:42:39 -0500

    Create  association methods on sequelize models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  OnDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through:{
+    model: ProductTag,
+  },
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:{
+    model: ProductTag
+  },
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
