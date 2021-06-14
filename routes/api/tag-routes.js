a28bf1f92d3318e3b8975a7fbdfc899c9845bfb2
Abdale Hassan <cabdale989@gmail.com>
2021-06-13 19:10:25 -0500

    add a post route to create a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,8 +38,14 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
